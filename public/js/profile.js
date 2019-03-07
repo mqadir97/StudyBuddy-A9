2b8e1da6aea4446c5c17e9811ce159f5cdad9d1d
mkhawark <mkhawar@ucsd.edu>
2019-03-06 21:00:23 -0800

    fb name

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,6 +4,6 @@ $(document).ready(function(){
 	console.log("ready");
 	$("#photo").attr("src", localStorage.getItem('photo'));
 
-	//$("#name").attr("src", localStorage.getItem('name'));
+	$("#name").text( localStorage.getItem('name'));
 
 }); //calls a function after the page is loadeds
